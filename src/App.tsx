6e1bb28b3078c9ec8d23243dcd2947628df1cedf
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-14 16:06:03 +0100

    Created create project popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CreateProjectPopup from "./components/CreateProjectPopup";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <main className="w-screen h-screen">
       <Sidebar />
+      <CreateProjectPopup />
     </main>
   );
 }
---
+++ src/components/CreateProjectPopup.tsx
@@ -0,0 +1,36 @@
+import { IoClose } from "react-icons/io5";
+
+const CreateProjectPopup = (): JSX.Element => {
+  return (
+    <div className="absolute top-0 left-0 w-screen h-screen bg-black/50 flex">
+      <div className="w-3/5 h-3/5 bg-white m-auto relative p-4 rounded-xl">
+        <IoClose
+          className="absolute right-4 top-4 cursor-pointer hover:text-black/50"
+          size={30}
+        />
+        <h1 className="text-2xl font-bold">Create Project</h1>
+        <div className="mt-4">
+          <p className="text-lg">Project Name:</p>
+          <input
+            type="text"
+            className="bg-[#EFEFEF] w-full text-lg px-2 py-0.5 rounded-lg mt-1"
+          />
+        </div>
+        <div className="mt-4">
+          <p className="text-lg">Project Description:</p>
+          <textarea className="bg-[#EFEFEF] w-full h-40 text-lg px-2 py-0.5 rounded-lg mt-1 resize-none" />
+        </div>
+        <div className="flex w-min ml-auto mt-10">
+          <button className="bg-blue-600 text-white w-40 mr-3 py-1 text-xl rounded-xl">
+            Cancel
+          </button>
+          <button className="bg-blue-600 text-white w-40 py-1 text-xl rounded-xl">
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateProjectPopup;
