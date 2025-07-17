4ccaac457453a673cf3910ffb078cd728857986b
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-17 23:30:23 +0100

    Created board list item layout

--- src/components/board/BoardList.tsx
+++ src/components/board/BoardList.tsx
@@ -1,10 +1,15 @@
+import BoardListItem from "./BoardListItem";
+
 const BoardList = (): JSX.Element => {
   return (
     <div className="grid grid-rows-[28px_auto_40px] w-[260px] h-full bg-blue-300 rounded-2xl px-4 py-2">
       {/* Title */}
       <h2 className="text-lg font-bold my-auto">List 1</h2>
       {/* Items */}
-      <div className="overflow-y-auto h-full"></div>
+      <div className="overflow-y-auto h-full">
+        <BoardListItem />
+        <BoardListItem />
+      </div>
       {/* Add Button */}
       <button className="text-left my-auto text-gray-400 select-none hover:text-gray-600">
         + Add Item
---
+++ src/components/board/BoardListItem.tsx
@@ -0,0 +1,9 @@
+const BoardListItem = (): JSX.Element => {
+  return (
+    <div className="bg-white px-2 py-1.5 mt-2 rounded-xl">
+      <p className="">Item 1</p>
+    </div>
+  );
+};
+
+export default BoardListItem;
