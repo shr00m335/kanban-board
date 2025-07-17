7e1b73eda19b7cfa57ef172a8eb44f94bcaaae78
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-17 18:20:25 +0100

    Created board layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import CreateProjectPopup from "./components/CreateProjectPopup";
 import Sidebar from "./components/Sidebar";
+import { Board } from "./components/board";
 
 function App() {
   const [showPopup, setShowPopup] = React.useState<boolean>(false);
 
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen grid grid-cols-[234px_auto]">
       <Sidebar onCreateClick={() => setShowPopup(true)} />
       {showPopup && (
         <CreateProjectPopup onCloseClick={() => setShowPopup(false)} />
       )}
+      <Board />
     </main>
   );
 }
---
+++ src/components/board/Board.tsx
@@ -0,0 +1,9 @@
+const Board = (): JSX.Element => {
+  return (
+    <div className="px-4 py-2.5">
+      <h1 className="text-2xl font-bold">Board 1</h1>
+    </div>
+  );
+};
+
+export default Board;
