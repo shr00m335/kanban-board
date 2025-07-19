756989a46ac3904e1ab7e07bddc9bc91a9b5850c
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-19 18:08:07 +0100

    Added function to write project header

---
+++ src-tauri/src/kanban/project.rs
@@ -0,0 +1,28 @@
+use crate::file_system::binary_writer::BinaryWriter;
+
+const FILE_VERSION: u8 = 0;
+
+fn write_project_header(bw: &mut BinaryWriter, name: &str, description: &str) {
+    // Version
+    bw.write_byte(FILE_VERSION);
+    // Name
+    bw.write_string_with_length(name, true);
+    // Description
+    bw.write_string_with_length(description, false);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_write_project_header() {
+        let mut bw: BinaryWriter = BinaryWriter::new();
+        write_project_header(&mut bw, "Test Name", "Test Description");
+        let expected_bytes: [u8; 28] = [
+            0x00, 0x09, 0x54, 0x65, 0x73, 0x74, 0x20, 0x4E, 0x61, 0x6D, 0x65, 0x10, 0x54, 0x65,
+            0x73, 0x74, 0x20, 0x44, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6F, 0x6E,
+        ];
+        assert_eq!(&expected_bytes, bw.as_bytes());
+    }
+}
