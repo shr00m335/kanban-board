d309052e282ed77f3ca62c76225ec326746ff8cb
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-19 22:58:47 +0100

    Added tauri error

--- src-tauri/src/errors/kanban_error.rs
+++ src-tauri/src/errors/kanban_error.rs
@@ -3,6 +3,7 @@ use std::{error::Error, fmt};
 #[derive(Debug)]
 pub enum KanbanError {
     IoError(std::io::Error),
+    TauriError(tauri::Error),
     ProjectError(String),
 }
 
@@ -10,6 +11,7 @@ impl fmt::Display for KanbanError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             KanbanError::IoError(err) => write!(f, "IO Error: {}", err),
+            KanbanError::TauriError(err) => write!(f, "Tauri Error: {}", err),
             KanbanError::ProjectError(msg) => write!(f, "Project Error: {}", msg),
         }
     }
@@ -19,6 +21,7 @@ impl Error for KanbanError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
             KanbanError::IoError(err) => Some(err),
+            KanbanError::TauriError(err) => Some(err),
             _ => None,
         }
     }
