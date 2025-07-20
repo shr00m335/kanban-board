e030ebde0610268955fc4ed6a417f688fc3f1850
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-20 23:58:13 +0100

    Added message banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import "./App.css";
 import CreateProjectPopup from "./components/CreateProjectPopup";
 import Sidebar from "./components/Sidebar";
@@ -6,14 +7,42 @@ import { Board } from "./components/board";
 
 function App() {
   const [showPopup, setShowPopup] = React.useState<boolean>(false);
+  const [showBanner, setShowBanner] = React.useState<boolean>(false);
+  const [bannerSuccess, setBannerSuccess] = React.useState<boolean>(true);
+  const [bannerMessage, setBannerMessage] = React.useState<string>("");
+
+  const showMessage = (success: boolean, message: string): void => {
+    setBannerSuccess(success);
+    setBannerMessage(message);
+    setShowBanner(true);
+    setTimeout(() => {
+      setShowBanner(false);
+    }, 2500);
+  };
 
   return (
-    <main className="w-screen h-screen grid grid-cols-[234px_auto]">
+    <main className="w-screen h-screen grid grid-cols-[234px_auto] overflow-y-hidden">
       <Sidebar onCreateClick={() => setShowPopup(true)} />
       {showPopup && (
-        <CreateProjectPopup onCloseClick={() => setShowPopup(false)} />
+        <CreateProjectPopup
+          showBanner={showMessage}
+          onCloseClick={() => setShowPopup(false)}
+        />
       )}
       <Board />
+      {/* Infomation Banner */}
+      <div
+        className={`w-min h-12 text-white absolute bottom-0 left-1/2 -translate-x-1/2 rounded-t-2xl flex items-center px-6 transition-transform duration-300 ${
+          showBanner ? "translate-y-0" : "translate-y-12"
+        } ${bannerSuccess ? "bg-green-500" : "bg-red-500"}`}
+      >
+        {bannerSuccess ? (
+          <FaCheckCircle size={28} />
+        ) : (
+          <FaTimesCircle size={28} />
+        )}
+        <p className="text-lg ml-4 text-nowrap">{bannerMessage}</p>
+      </div>
     </main>
   );
 }
