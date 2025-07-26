ca610569c7d9993ee35cf839e9907cce5d67cf7e
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-26 14:15:09 +0100

    Added function to read all boards

--- src-tauri/src/kanban/board.rs
+++ src-tauri/src/kanban/board.rs
@@ -51,6 +51,15 @@ fn read_board(br: &mut BinaryReader) -> Result<Board, KanbanError> {
     Ok(Board { name, items })
 }
 
+pub(crate) fn read_all_boards(br: &mut BinaryReader) -> Result<Vec<Board>, KanbanError> {
+    let boards_count = br.next_leb128_number()?;
+    let mut boards = Vec::<Board>::new();
+    for _ in 0..boards_count {
+        boards.push(read_board(br)?);
+    }
+    Ok(boards)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -121,5 +130,38 @@ mod test {
         let result = read_board(&mut br);
         assert!(result.is_ok());
         assert_eq!(expected_board, result.unwrap());
+    }
+
+    #[test]
+    fn test_read_all_boards() {
+        let mut br = BinaryReader::new(&[
+            0x03, 0x0C, 0x54, 0x65, 0x73, 0x74, 0x20, 0x42, 0x6F, 0x61, 0x72, 0x64, 0x20, 0x31,
+            0x03, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20, 0x31, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20,
+            0x32, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20, 0x33, 0x0C, 0x54, 0x65, 0x73, 0x74, 0x20,
+            0x42, 0x6F, 0x61, 0x72, 0x64, 0x20, 0x32, 0x02, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20,
+            0x31, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20, 0x32, 0x0C, 0x54, 0x65, 0x73, 0x74, 0x20,
+            0x42, 0x6F, 0x61, 0x72, 0x64, 0x20, 0x33, 0x01, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20,
+            0x31,
+        ]);
+        let expected_boards = [
+            Board {
+                name: "Test Board 1".to_string(),
+                items: ["Item 1", "Item 2", "Item 3"]
+                    .map(|s| s.to_string())
+                    .to_vec(),
+            },
+            Board {
+                name: "Test Board 2".to_string(),
+                items: ["Item 1", "Item 2"].map(|s| s.to_string()).to_vec(),
+            },
+            Board {
+                name: "Test Board 3".to_string(),
+                items: ["Item 1"].map(|s| s.to_string()).to_vec(),
+            },
+        ]
+        .to_vec();
+        let result = read_all_boards(&mut br);
+        assert!(result.is_ok());
+        assert_eq!(expected_boards, result.unwrap());
     }
 }
