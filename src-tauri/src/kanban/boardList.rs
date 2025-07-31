225eb69275af7f893f0df67943c3f2786c29fc14
shr00m335 <zcabswu@ucl.ac.uk>
2025-07-31 23:22:03 +0100

    Added function to read all board lists

--- src-tauri/src/kanban/boardList.rs
+++ src-tauri/src/kanban/boardList.rs
@@ -45,11 +45,20 @@ pub(crate) fn read_board_list(br: &mut BinaryReader) -> Result<BoardList, Kanban
     let title: String = br.next_string(true)?;
     // Board items
     let items_count: usize = br.next_leb128_number()?;
-    let mut items: Vec<String> = Vec::new();
+    let mut items: Vec<String> = Vec::with_capacity(items_count);
     for _ in 0..items_count {
         items.push(br.next_string(false)?);
     }
     Ok(BoardList { title, items })
+}
+
+pub(crate) fn read_all_board_lists(br: &mut BinaryReader) -> Result<Vec<BoardList>, KanbanError> {
+    let lists_count = br.next_leb128_number()?;
+    let mut lists = Vec::with_capacity(lists_count);
+    for _ in 0..lists_count {
+        lists.push(read_board_list(br)?);
+    }
+    Ok(lists)
 }
 
 #[cfg(test)]
@@ -127,5 +136,37 @@ mod test {
         let result = read_board_list(&mut br);
         assert!(result.is_ok());
         assert_eq!(expected_list, result.unwrap());
+    }
+
+    #[test]
+    fn test_read_all_board_lists() {
+        let test_data = [
+            0x03, 0x0C, 0x54, 0x65, 0x73, 0x74, 0x20, 0x42, 0x6F, 0x61, 0x72, 0x64, 0x20, 0x31,
+            0x03, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20, 0x31, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20,
+            0x32, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20, 0x33, 0x0C, 0x54, 0x65, 0x73, 0x74, 0x20,
+            0x42, 0x6F, 0x61, 0x72, 0x64, 0x20, 0x32, 0x02, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20,
+            0x31, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20, 0x32, 0x0C, 0x54, 0x65, 0x73, 0x74, 0x20,
+            0x42, 0x6F, 0x61, 0x72, 0x64, 0x20, 0x33, 0x01, 0x06, 0x49, 0x74, 0x65, 0x6D, 0x20,
+            0x31,
+        ];
+        let test_list_1 = BoardList {
+            title: "Test Board 1".to_string(),
+            items: ["Item 1", "Item 2", "Item 3"]
+                .map(|s| s.to_string())
+                .to_vec(),
+        };
+        let test_list_2 = BoardList {
+            title: "Test Board 2".to_string(),
+            items: ["Item 1", "Item 2"].map(|s| s.to_string()).to_vec(),
+        };
+        let test_list_3 = BoardList {
+            title: "Test Board 3".to_string(),
+            items: ["Item 1"].map(|s| s.to_string()).to_vec(),
+        };
+        let expected_lists = [test_list_1, test_list_2, test_list_3];
+        let mut br = BinaryReader::new(&test_data);
+        let result = read_all_board_lists(&mut br);
+        assert!(result.is_ok());
+        assert_eq!(expected_lists.to_vec(), result.unwrap());
     }
 }
