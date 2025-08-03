0744e43851ab95e9c9e087c19b65870421fa2485
shr00m335 <zcabswu@ucl.ac.uk>
2025-08-03 15:18:07 +0100

    Added functions to delete items

--- src/components/board/BoardListItem.tsx
+++ src/components/board/BoardListItem.tsx
@@ -1,5 +1,6 @@
 import { useAtom } from "jotai";
 import React from "react";
+import { FaTrash } from "react-icons/fa";
 import { BoardListModel, BoardModel } from "../../models/project";
 import {
   draggingItemAtom,
@@ -33,8 +34,10 @@ const BoardListItem = ({
   const [mousePos, setMousePos] = React.useState<number[]>([0, 0]);
   const [openedBoard, setOpenedBoard] = useAtom(openedBoardAtom);
   const [isEditingItem, setIsEditingItem] = React.useState<boolean>(false);
+  const [isDeleteOverlap, setIsDeleteOverlap] = React.useState<boolean>(false);
 
   const itemRef = React.useRef<HTMLParagraphElement>(null);
+  const deleteAreaRef = React.useRef<HTMLDivElement>(null);
 
   let mouseHoldTimer: number | null = null;
 
@@ -72,7 +75,18 @@ const BoardListItem = ({
     if (e.button !== 0) return;
     setIsDragging(false);
     if (!openedBoard) return;
-    if (draggingItem !== null) {
+    if (isDeleteOverlap) {
+      const newBoard = {
+        ...openedBoard,
+        lists: openedBoard.lists.map((list, idx) =>
+          idx === boardListIndex
+            ? { ...list, items: list.items.filter((_, i) => i !== itemIndex) }
+            : list
+        ),
+      };
+      setOpenedBoard(newBoard);
+      console.log("Deleted");
+    } else if (draggingItem !== null) {
       let newBoard = {
         ...openedBoard,
         lists: openedBoard.lists.map((list, idx) =>
@@ -97,27 +111,46 @@ const BoardListItem = ({
             : list
         ),
       };
+
       setOpenedBoard(newBoard);
     }
     setDraggingItem(null);
     setDraggingItemLocation({ listIndex: -1, itemIndex: -1 });
   };
 
+  const checkDeleteOverlap = (x: number, y: number): void => {
+    setIsDeleteOverlap(
+      x >= deleteArea.x1 &&
+        x <= deleteArea.x2 &&
+        y >= deleteArea.y1 &&
+        y <= deleteArea.y2
+    );
+  };
+
   const handleMouseMove = (e: React.MouseEvent) => {
     const pos = [e.clientX - 112, e.clientY - 18];
     setMousePos(pos);
-    const listLocation =
-      pos[0] < firstItemLocation[0]
-        ? -1
-        : Math.floor((pos[0] - firstItemLocation[0]) / 270);
-    const itemLocation =
-      pos[1] < firstItemLocation[1]
-        ? -1
-        : Math.floor((pos[1] - firstItemLocation[1]) / 40);
-    setDraggingItemLocation({
-      listIndex: listLocation,
-      itemIndex: itemLocation,
-    });
+    if (pos[1] < window.innerHeight - 100) {
+      const listLocation =
+        pos[0] < firstItemLocation[0]
+          ? -1
+          : Math.floor((pos[0] - firstItemLocation[0]) / 270);
+      const itemLocation =
+        pos[1] < firstItemLocation[1]
+          ? -1
+          : Math.floor((pos[1] - firstItemLocation[1]) / 40);
+      setDraggingItemLocation({
+        listIndex: listLocation,
+        itemIndex: itemLocation,
+      });
+      setIsDeleteOverlap(false);
+    } else {
+      checkDeleteOverlap(e.clientX, e.clientY);
+      setDraggingItemLocation({
+        listIndex: -1,
+        itemIndex: -1,
+      });
+    }
   };
 
   const handleItemDbClick = (
@@ -179,8 +212,43 @@ const BoardListItem = ({
     ]);
   }, [itemRef]);
 
+  let deleteArea: {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+  } = { x1: 0, y1: 0, x2: 0, y2: 0 };
+
+  React.useEffect(() => {
+    if (deleteAreaRef.current === null) return;
+    const rect = deleteAreaRef.current.getBoundingClientRect();
+    deleteArea = {
+      x1: rect.left,
+      y1: rect.top,
+      x2: rect.right,
+      y2: rect.bottom,
+    };
+  });
+
   return (
     <>
+      {isDragging && (
+        <div
+          className="w-screen h-screen absolute top-0 left-0 z-10"
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          <div
+            ref={deleteAreaRef}
+            className="absolute flex justify-center items-center text-white bg-red-500 rounded-t-xl w-[300px] h-10 bottom-0 left-1/2 hover:-z-10"
+            style={{
+              opacity: isDeleteOverlap ? 1 : 0.5,
+            }}
+          >
+            <FaTrash size={24} />
+          </div>
+        </div>
+      )}
       <p
         ref={itemRef}
         className={`bg-white w-[224px] px-2 py-1.5 mt-2 rounded-xl whitespace-pre-line ${
@@ -199,13 +267,6 @@ const BoardListItem = ({
       >
         {item}
       </p>
-      {isDragging && (
-        <div
-          className="w-screen h-screen absolute top-0 left-0"
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-        />
-      )}
     </>
   );
 };
